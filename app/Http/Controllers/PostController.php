26301d23867bb0f8edd511ea05945740db53f03e
nwkwok <nwkwok@gmail.com>
2021-02-07 22:04:40 -0800

    add ability to delete posts and add policies to create right permissions for deleting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with(['user', 'likes'])->paginate(20); // returns collection
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(20); // returns collection
         return view('posts.index', [
             'posts' => $posts
         ]);
@@ -23,6 +23,13 @@ class PostController extends Controller
 
         $request->user()->posts()->create($request->only('body'));
 
+        return back();
+    }
+
+    public function destroy(Post $post)
+    {
+        $this->authorize('delete', $post);
+        $post->delete();
         return back();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,6 +23,15 @@
                 <div class="mb-4">
                     <a href="" class="font-bold">{{$post->user->name}}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
                     <p class="mb-2">{{$post->body}}</p>
+
+                    @can('delete', $post)
+                        <form action="{{ route('posts.destroy', $post)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="text-blue-500">Delete</button>
+                        </form>
+                    @endcan
+                    
                     <div class="flex items-center">
                         @auth
                             @if (!$post->likedBy(auth()->user()))
@@ -37,6 +46,9 @@
                                 <button type="submit" class="text-blue-500">Unlike</button>
                             </form>
                             @endif
+
+                    
+
                         @endauth
                         <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                     </div>
